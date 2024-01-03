7e51cb135882e604eed56b236dd9853874e09f52
Trang <105605945+Trang1908@users.noreply.github.com>
2024-01-03 18:05:48 +0000

    Add files via upload

---
+++ T03 - Control Structures - If, Elif, Else and the Boolean Data type/age-quiz.py
@@ -0,0 +1,16 @@
+# Request user's age and store it in an integer variable called age
+age = int(input("Please enter your age: "))
+
+
+if age > 100:
+    print("Sorry, you're dead.")
+elif age >= 65:
+    print("Enjoy your retirement!")
+elif age >= 40:
+    print("You're over the hill.")
+elif age == 21:
+    print("Congrats on your 21st!")
+elif age < 13:
+    print("You qualify for the kiddie discount.")
+else:
+    print("Age is but a number.")
